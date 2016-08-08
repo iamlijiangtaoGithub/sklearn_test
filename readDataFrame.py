174022cb4bfe7d33946a26c93edf3123754ada40
lijiangtao <404066449@qq.com>
2016-08-08 15:31:11 +0800

    update readDataFrame.py,warpper function definition

--- readDataFrame.py
+++ readDataFrame.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 #coding=utf8
 import pandas as pd
-reader = pd.read_csv('/home/hdp_teu_search/ljt/sample/ershouche_sample.txt',header=None,delimiter="\t", iterator=True)
-loop = True
-chunkSize = 100000
-chunks = []
-while loop:
-    try:
-        chunk = reader.get_chunk(chunkSize)
-        chunks.append(chunk)
-    except StopIteration:
-        loop = False
+
+def readBigData(filePath,delim,header=None):
+    reader = pd.read_csv(filePath,header=None,delimiter=delim, iterator=True)
+    loop = True
+    chunkSize = 100000
+    chunks = []
+    while loop:
+        try:
+            chunk = reader.get_chunk(chunkSize)
+            chunks.append(chunk)
+        except StopIteration:
+            loop = False
         print "Iteration is stopped."
-df = pd.concat(chunks, ignore_index=True)
+    df = pd.concat(chunks, ignore_index=True)
+    return df
